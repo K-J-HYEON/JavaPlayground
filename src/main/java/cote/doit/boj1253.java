ac154f66909e379d27ca32690e7aebdf4fc4ab08
K-J-HYEON <kjh950601@naver.com>
2022-07-28 22:44:00 +0900

    javaAlgorithm

---
+++ src/main/java/cote/doit/boj1253.java
@@ -0,0 +1,14 @@
+package cote.doit;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class boj1253 {
+    public static void main(String[] args) {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        int Result = 0;
+
+
+    }
+}
