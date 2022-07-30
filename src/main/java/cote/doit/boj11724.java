06ed52ece603a767b473deaaa6f5caf45fad67b1
K-J-HYEON <kjh950601@naver.com>
2022-07-30 14:52:52 +0900

    javGrammer

--- src/main/java/cote/doit/boj11724.java
+++ src/main/java/cote/doit/boj11724.java
@@ -1,7 +1,11 @@
 package cote.doit;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class boj11724 {
     static ArrayList<Integer>[] A;
     static boolean visited[];
@@ -12,19 +16,20 @@ public class boj11724 {
         int m = Integer.parseInt(st.nextToken());
         A = new ArrayList[n + 1];
         visited = new boolean[n + 1];
-        for (int i = 1; i < n + 1; i++) { // 인접 리스트 초기화
+        for (int i = 1; i < n + 1; i++) {
             A[i] = new ArrayList<Integer>();
         }
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-            A[s].add(e); // 양 방향 간선이므로 양쪽으로 간선을 더 해준다
+
+            A[s].add(e);
             A[e].add(s);
         }
         int count = 0;
         for (int i = 1; i < n + 1; i++) {
-            if (!visited[i]) { // 미 방문한 정점이 없을 때까지 반복
+            if (!visited[i]) {
                 count++;
                 DFS(i);
             }
@@ -37,7 +42,7 @@ public class boj11724 {
         }
         visited[v] = true;
         for (int i : A[v]) {
-            if (visited[i] == false) { // 연결 정점 중 방문하지 않았던 정점만 탐색함
+            if (visited[i] == false) { // 연결 노드 중 방문하지 않았던 노드만 탐색하기
                 DFS(i);
             }
         }
