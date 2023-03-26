96ce29f21fc109b41c07129223403b83461804d1
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-03-26 13:59:29 +0900

    BridgeCount DP

---
+++ src/main/java/cote/doit/BridgeCount.java
@@ -0,0 +1,30 @@
+package cote.doit;
+
+import java.util.Scanner;
+
+public class BridgeCount {
+    static long[][] D;
+
+    public static void main(String[] args) {
+        D = new long[31][31];
+        for (int i = 0; i <= 30; i++) {
+            D[i][i] = i;
+            D[i][0] = 1;
+            D[i][i] = 1;
+        }
+
+        for (int i = 2; i <= 30; i++) {
+            for (int j = 1; j < i; j++) {
+                D[i][j] = D[i - 1][j] + D[i - 1][j - 1];
+            }
+        }
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            System.out.println(D[M][N]);
+        }
+    }
+}
